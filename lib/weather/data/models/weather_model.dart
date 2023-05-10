6b355bbb156387a05d88e09cafab344c7ceac61d
saci zakaria <sacideveloppeur@gmail.com>
2023-05-10 22:51:43 +0200

    extends Weather

--- lib/weather/data/models/weather_model.dart
+++ lib/weather/data/models/weather_model.dart
@@ -1,1 +1,11 @@
-class WeatherModel {}
+import 'package:clean_architecture_elgendy/weather/domain/entities/weather.dart';
+
+class WeatherModel extends Weather {
+  WeatherModel(
+    super.id,
+    super.cityName,
+    super.main,
+    super.description,
+    super.pressure,
+  );
+}
