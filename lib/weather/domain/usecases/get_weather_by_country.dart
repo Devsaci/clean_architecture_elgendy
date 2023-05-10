dcfe596dcb77d493d667f9f242f032b1d66eb50b
saci zakaria <sacideveloppeur@gmail.com>
2023-05-10 22:39:01 +0200

    final BaseWeatherRepository repository;

--- lib/weather/domain/usecases/get_weather_by_country.dart
+++ lib/weather/domain/usecases/get_weather_by_country.dart
@@ -1,1 +1,7 @@
-class GetWeatherByCountryName{}
+import '../repository/base_weather_repository.dart';
+
+class GetWeatherByCountryName{
+  final BaseWeatherRepository repository;
+
+  GetWeatherByCountryName(this.repository);
+}
