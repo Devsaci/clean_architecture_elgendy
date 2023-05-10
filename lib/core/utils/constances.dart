43153a8d85fdbfac9d7a870ed9f4108ff39651db
saci zakaria <sacideveloppeur@gmail.com>
2023-05-10 22:10:09 +0200

    respons.json

--- lib/core/utils/constances.dart
+++ lib/core/utils/constances.dart
@@ -2,3 +2,7 @@ class AppConstance{
   static const String baseUrl = "https://api.openweathermap.org/data/2.5";
   static const String appKey = "18f6ee256a29b89293434327c027a685";
 }
+
+// https://api.openweathermap.org/data/2.5/weather?q=amiens&appid=18f6ee256a29b89293434327c027a685
+// 20230510213629
+// https://api.openweathermap.org/data/2.5/weather?q=amiens&appid=18f6ee256a29b89293434327c027a685
