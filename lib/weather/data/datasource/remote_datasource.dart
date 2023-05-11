c5c63a2a5007c68ab3e10c35e0e3afdcd15820e4
saci zakaria <sacideveloppeur@gmail.com>
2023-05-11 09:55:56 +0200

    return WeatherModel.fromJson(json.decode(response.data));

--- lib/weather/data/datasource/remote_datasource.dart
+++ lib/weather/data/datasource/remote_datasource.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
@@ -14,8 +16,11 @@ class RemoteDataSource implements BaseRemoteDataSource {
   Future<WeatherModel?> getWeatherByCountryName(String countryName) async {
     try {
       var response = await Dio().get('url API');
+      if (kDebugMode) {print(response);}
+      return WeatherModel.fromJson(json.decode(response.data));
     } catch (e) {
       if (kDebugMode) {print(e);}
     }
+    return null;
   }
 }
