11e4dc8310fe6b6a20cf0da923b139665df1cae8
saci zakaria <sacideveloppeur@gmail.com>
2023-05-11 09:53:10 +0200

    await Dio().get('url API')

--- lib/weather/data/datasource/remote_datasource.dart
+++ lib/weather/data/datasource/remote_datasource.dart
@@ -1,12 +1,21 @@
+
+
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
 import '../models/weather_model.dart';
 
 abstract class BaseRemoteDataSource {
-  Future<WeatherModel> getWeatherByCountryName(String countryName);
+  Future<WeatherModel?> getWeatherByCountryName(String countryName);
 }
- class RemoteDataSource implements BaseRemoteDataSource{
+
+class RemoteDataSource implements BaseRemoteDataSource {
   @override
-  Future<WeatherModel> getWeatherByCountryName(String countryName) {
-    // TODO: implement getWeatherByCountryName
-    throw UnimplementedError();
+  Future<WeatherModel?> getWeatherByCountryName(String countryName) async {
+    try {
+      var response = await Dio().get('url API');
+    } catch (e) {
+      if (kDebugMode) {print(e);}
+    }
   }
 }
