586cab3fc7d446bfdc03c6bc34aa3af1151605ec
saci zakaria <sacideveloppeur@gmail.com>
2023-05-11 09:20:43 +0200

    class RemoteDataSource implements BaseRemoteDataSource

--- lib/weather/data/datasource/remote_datasource.dart
+++ lib/weather/data/datasource/remote_datasource.dart
@@ -3,3 +3,10 @@ import '../models/weather_model.dart';
 abstract class BaseRemoteDataSource {
   Future<WeatherModel> getWeatherByCountryName(String countryName);
 }
+ class RemoteDataSource implements BaseRemoteDataSource{
+  @override
+  Future<WeatherModel> getWeatherByCountryName(String countryName) {
+    // TODO: implement getWeatherByCountryName
+    throw UnimplementedError();
+  }
+}
