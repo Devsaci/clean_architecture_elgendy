b187d243f7b66375bcc07b7443daa86e4b53d078
saci zakaria <sacideveloppeur@gmail.com>
2023-05-11 11:08:16 +0200

    final RemoteDataSource remoteDataSource;

--- lib/weather/data/repository/weather_repository.dart
+++ lib/weather/data/repository/weather_repository.dart
@@ -1,1 +1,16 @@
-class WeatherRepository {}
+import 'package:clean_architecture_elgendy/weather/data/datasource/remote_datasource.dart';
+import 'package:clean_architecture_elgendy/weather/domain/entities/weather.dart';
+import 'package:clean_architecture_elgendy/weather/domain/repository/base_weather_repository.dart';
+
+class WeatherRepository implements BaseWeatherRepository{
+
+  final RemoteDataSource remoteDataSource;
+
+  WeatherRepository(this.remoteDataSource);
+
+  @override
+  Future<Weather> getWeatherByCityName(String cityName) {
+    // TODO: implement getWeatherByCityName
+    throw UnimplementedError();
+  }
+}
